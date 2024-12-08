e1ff58e32319d54e577b0ba205c92ddbb9b487b3
VTrT <dawid@dbernat.pl>
2024-12-08 19:51:58 +0100

    Advent of Code 2024

--- advent-of-code/2.report-checker.py
+++ advent-of-code/2.report-checker.py
@@ -1,4 +1,4 @@
-'''
+"""
 The unusual data (your puzzle input) consists of many reports, one report per line. Each report is a list of numbers called levels that are separated by spaces. For example:
 
 7 6 4 2 1
@@ -15,16 +15,7 @@ The engineers are trying to figure out which reports are safe.
 
 The levels are either all increasing or all decreasing.
 Any two adjacent levels differ by at least one and at most three.
-'''
-reports_list = [
-    [7, 6, 4, 2, 1],
-    [1, 2, 7, 8, 9],
-    [9, 7, 6, 2, 1],
-    [1, 3, 2, 4, 5],
-    [8, 6, 4, 4, 1],
-    [1, 3, 6, 7, 9]
-]
-
+"""
 
 def validate_report(report):
     if report[0] > report[1]:
@@ -69,7 +60,15 @@ def pretty_report(list_reports):
         print(f"{report['Report']} - {report["Validation"]}")
 
 
+reports_list = [
+    [7, 6, 4, 2, 1],
+    [1, 2, 7, 8, 9],
+    [9, 7, 6, 2, 1],
+    [1, 3, 2, 4, 5],
+    [8, 6, 4, 4, 1],
+    [1, 3, 6, 7, 9]
+]
+
 validated_reports_list = check_reports(reports_list)
+
 pretty_report(validated_reports_list)
-
-
